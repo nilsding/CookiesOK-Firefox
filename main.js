f9fcc315b8c729c397992dc64a129d033d68d5df
Nick Sulkers <nicksulkers@beachconcepts.nl>
2016-02-13 15:48:53 +0100

    implement localization and fix bug where DB is not yet loaded on first page load.

--- main.js
+++ main.js
@@ -2,21 +2,29 @@ var self = require("sdk/self");
 var data = self.data;
 var pageMod = require("sdk/page-mod");
 var ss = require("sdk/simple-storage");
+var sp = require("sdk/simple-prefs");
 var request = require("sdk/request").Request;
+var _ = require("sdk/l10n").get;
 
 var database = {};
 
-function downloadDatabase() {
-	request({
-		url: "https://cookiesok.com/5/database",
-		onComplete: function (response) {
-			var result = JSON.parse(response.text);
-			if(result.success) {
-				database.websites = result.data;
-				database.updated = new Date();
+var reportContextMenuItem;
+
+function resetContextMenuItem(){
+	if(reportContextMenuItem)
+		reportContextMenuItem.destroy();
+
+	if(sp.prefs.contextmenu){
+		var contextMenu = require("sdk/context-menu");
+		reportContextMenuItem = contextMenu.Item({
+			label: _("context_menu_report_text"),
+			contentScript: 'self.on("click", function () { self.postMessage(location.href); });',
+			onMessage: function(url){
+				url = url.split(":").join("_-_"); //part 1/2 of ugly workaround, if the url contains a ":" (encoded %5A) firefox will give an error upon loading it. Unsure why, bug?
+				require("sdk/tabs").open(data.url("pages/report.html?url=" + encodeURIComponent(url)));
 			}
-		}
-	}).get();
+		});
+	}
 }
 
 function isTooOld(date) {
@@ -27,39 +35,50 @@ function isTooOld(date) {
 	return timePassed > msInDay;
 }
 
-if (!ss.storage.database || isTooOld(ss.storage.database.updated))
-	downloadDatabase();
-else
-	database = ss.storage.database;
-
-pageMod.PageMod({
-	include: "*",
-	contentScriptFile: [
-		data.url("scripts/cookiesok.js")
-	],
-	contentScriptOptions: {
-		version: self.version
-	},
-	onAttach: function(worker){
-		worker.port.on('getDomainOrders', function(hostname){
-			if (hostname.indexOf('www.') === 0)
-				hostname = hostname.substr(4);
-
-			var orders = database.websites[hostname];
-			if (!orders && hostname.match('.')) {
-				var tmpHostname = hostname.split(".");
-				tmpHostname[0] = '*';
-				tmpHostname = tmpHostname.join('.');
-				orders = database.websites[tmpHostname];
+function downloadDatabase(callback) {
+	request({
+		url: "https://cookiesok.com/5/database",
+		onComplete: function (response) {
+			var result = JSON.parse(response.text);
+			if(result.success) {
+				database.websites = result.data;
+				database.updated = new Date();
+				callback && callback();
 			}
+		}
+	}).get();
+}
 
-			if(orders)
-				worker.port.emit('getDomainOrders', {success: true, orders: orders});
-			else
-				worker.port.emit('getDomainOrders', {success: false});
-		});
-	}
-});
+function databaseReady(){
+	pageMod.PageMod({
+		include: "*",
+		contentScriptFile: [
+			data.url("scripts/cookiesok.js")
+		],
+		contentScriptOptions: {
+			version: self.version
+		},
+		onAttach: function(worker){
+			worker.port.on('getDomainOrders', function(hostname){
+				if(hostname.indexOf('www.') === 0)
+					hostname = hostname.substr(4);
+
+				var orders = database.websites[hostname];
+				if(!orders && hostname.match('.')){
+					var tmpHostname = hostname.split(".");
+					tmpHostname[0] = '*';
+					tmpHostname = tmpHostname.join('.');
+					orders = database.websites[tmpHostname];
+				}
+
+				if(orders)
+					worker.port.emit('getDomainOrders', {success: true, orders: orders});
+				else
+					worker.port.emit('getDomainOrders', {success: false});
+			});
+		}
+	});
+}
 
 pageMod.PageMod({
 	include: "*",
@@ -89,25 +108,14 @@ var httpRequestObserver =
 };
 httpRequestObserver.register();
 
-var reportContextMenuItem;
-function resetContextMenuItem(){
-	if(reportContextMenuItem)
-		reportContextMenuItem.destroy();
-
-	if(require("sdk/simple-prefs").prefs.contextmenu){
-		var contextMenu = require("sdk/context-menu");
-		reportContextMenuItem = contextMenu.Item({
-			label: "Report to CookiesOK",
-			contentScript: 'self.on("click", function () { self.postMessage(location.href); });',
-			onMessage: function(url){
-				url = url.split(":").join("_-_"); //part 1/2 of ugly workaround, if the url contains a ":" (encoded %5A) firefox will give an error upon loading it. Unsure why, bug?
-				require("sdk/tabs").open(data.url("pages/report.html?url=" + encodeURIComponent(url)));
-			}
-		});
-	}
-}
-
-require("sdk/simple-prefs").on("", function(){
+sp.on("", function(){
 	resetContextMenuItem();
 });
 resetContextMenuItem();
+
+if (!ss.storage.database || isTooOld(ss.storage.database.updated))
+	downloadDatabase(databaseReady);
+else{
+	database = ss.storage.database;
+	databaseReady();
+}
